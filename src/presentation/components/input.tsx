c88ee1ea2df5163092192d2933f9782300722409
Nicolas Lavoura <nicolas.lavoura@teamsoft.com.br>
2025-08-17 02:07:31 -0300

    feat: create Input component

---
+++ src/presentation/components/input.tsx
@@ -0,0 +1,33 @@
+import type { InputHTMLAttributes } from "react";
+
+type InputProperties = {
+  title?: string;
+} & Pick<
+  InputHTMLAttributes<HTMLInputElement>,
+  "disabled" | "onChange" | "placeholder" | "value"
+>;
+
+function Input({
+  disabled,
+  onChange,
+  placeholder,
+  title,
+  value,
+}: InputProperties) {
+  return (
+    <div>
+      {title ? <label htmlFor={title}>{title}</label> : undefined}
+
+      <input
+        className="border-primary-600 w-full rounded-lg border px-2 py-1 disabled:bg-neutral-200"
+        disabled={disabled}
+        id={title}
+        onChange={onChange}
+        placeholder={placeholder}
+        value={value}
+      />
+    </div>
+  );
+}
+
+export { Input };
