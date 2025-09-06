e7c7f7ccb3d3b4b3a49cc54f2adda28cac5facab
Nicolas Lavoura <nicolas.lavoura@teamsoft.com.br>
2025-09-06 07:04:01 -0300

    feat: create list error component

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,6 +1,7 @@
 export * from "./button";
 export * from "./dropdown";
 export * from "./input";
+export * from "./list-error";
 export * from "./menu";
 export * from "./no-items";
 export * from "./page-header";
---
+++ src/presentation/components/list-error/list-error.tsx
@@ -0,0 +1,9 @@
+function ListError() {
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <span>Erro na listagem</span>
+    </div>
+  );
+}
+
+export { ListError };
